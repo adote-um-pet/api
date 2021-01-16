2f66a8609243f734caf94e9e4da1dcabba103fe5
Gleison Batista Soares <gleisonbs@gmail.com>
2021-01-16 14:59:48 -0300

    chore: add jest threshold config

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,18 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
   roots: [
     '<rootDir>/src'
   ],
+  modulePathIgnorePatterns: ["protocols"],
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
